4b870b2b81e068202cab91784dc1fde1a10e968d
Naïké Dramé <naike.drame.dev@gmail.com>
2025-04-01 13:39:40 +0200

    added fake test

---
+++ Labo.Test/TestFail1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo.Test
+{
+    public class TestFail1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(42, 1);
+        }
+    }
+}
